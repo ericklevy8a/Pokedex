4513f990fd8b60dcb72c27afecc106d249dfd335
Erick Levy <ericklevy@gmail.com>
2022-02-26 11:56:17 -0600

    Better responsive design and shadows

--- pokedex.js
+++ pokedex.js
@@ -408,4 +408,13 @@ function prepareTitle(str) {
     return tokens.join(' ');
 }
 
+// Top-nav initialization
+function toggleMenu() {
+    const menu = document.getElementById('top-menu');
+    const icon = document.getElementById('toggle-icon');
+    menu.classList.toggle('active');
+    icon.innerText = (icon.innerText == 'menu') ? 'close' : 'menu';
+}
+document.getElementById('toggle-icon').addEventListener('click', toggleMenu);
+
 // End of code.
